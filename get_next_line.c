1dd851053ac7e99ce072c3f7bedb3b652b1090ce
Alexandre Tomasi <atomasi@c2r5s4.42lausanne.ch>
2024-10-18 11:56:00 +0200

    Avancement get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 14:09:41 by atomasi           #+#    #+#             */
-/*   Updated: 2024/10/17 17:13:51 by atomasi          ###   ########.fr       */
+/*   Updated: 2024/10/18 11:55:14 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,56 @@
 
 int		check_buffer(char *buffer, static char **stash, char *line, int iline)
 {
-	int i;
+	int ibuff;
+	int istash;
+	int max;
 
-	i = 0;
+	ibuff = 0;
+	istash = 0;
+	max = ft_strlen(line) + ft_strlen(buff) + 1;
+	line = malloc(sizeof(char) * max);
+	if (line == NULL)
+		return (NULL);
+	line[max] = '\0';
 	while (buffer[i])
 	{
-		if (buffer[i] == '\n')
+		if (buffer[ibuff] == '\n')
 		{
-			while (buffer[i])
+			stash[fd] = malloc(sizeof(char) * (BUFFER_SIZE - ft_strlen(buffer)));
+			if (stash[fd] == NULL)
+				return (NULL);
+			while (buffer[ibuff])
 				{
-					stash[fd][i] = buffer[i];
-					i++;
+					stash[fd][istash] = buffer[ibuff];
+					ibuff++;
+					istash++;
+					return (0);
 				}
 		}
-		line[i] = buffer[i];
-		i++;
+		line = ft_strjoin(line, buffer);
 	}
 	return (1);
 }
 
+int	add_stash(char *stash, char *line)
+{
+	int iline;
+	int istash;
+
+	iline = 0;
+	istash = 0;
+	while (stash[istash])
+	{
+		line[iline] = stash[fd][istash];
+		istash++;
+		iline++;
+	}
+	return (iline);
+}
+
 char	*get_next_line(int fd)
 {
-	static char **stash;
+	static char *stash[1024];
 	char buffer[BUFFER_SIZE + 1];
 	char *line;
 	int isread;
@@ -43,17 +71,17 @@ char	*get_next_line(int fd)
 
 	iline = 0;
 	isread = 1;
-	while (isread)
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	if (stash[fd])
+		iline = add_stash(stash, line);
+	while (isread) // reflexion en cours car read lit actuellement tout le fichier
 	{
 		isread = read(fd, buffer, BUFFER_SIZE);
-		if (!isread || !buffer)
-			return (-1);
-		check_buffer(buffer, stash, line, iline);
-
-
-
+		if (isread < 0 || !buffer)
+			return (NULL);
+		buffer[BUFFER_SIZE + 1] = '\0';
+		check_buffer(buffer, stash, line, iline); // On pourait dire que si cette fonction renvoie 0 alors isread = 0
 	}
-
-
-
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,45 @@
 /*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 14:10:04 by atomasi           #+#    #+#             */
-/*   Updated: 2024/10/17 14:25:28 by atomasi          ###   ########.fr       */
+/*   Updated: 2024/10/18 10:08:15 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strjoin(char *s1, char *s2)
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
 {
 	int i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+	int i1;
+	int i2;
+	char *res;
+
+	i1 = 0;
+	i2 = 0;
+	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (res == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		res[i1] = s1[i1];
+		i1++;
+	}
+	while (s2[s2] && s2[i2] != '\n')
+	{
+		res[i1] = s2[i2];
+		i1++;
+		i2++;
+	}
+	retrun (res);
 }
